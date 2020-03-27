645727a7a0854c3bca1447c750e199e3e9431083
Eloghosa Ikponmwoba <eloghosaefficiency@gmail.com>
2020-03-27 12:03:37 +0100

    Javascript first task

---
+++ JsTask1.js
@@ -0,0 +1,28 @@
+
+console.log("First Js Code")
+
+let name = "Elotech"
+
+const courses = ["python","HTML","CSS","Design","JavaScript","GO"]
+
+console.log(name)
+console.log(courses)
+
+const numbers = []
+if (courses.length%2 == 0){
+for (let i = 0; i <= 200; i++) {
+    if (i%2 === 0){
+        console
+        numbers.push(i)
+    }
+   }
+}
+else {
+    for (let i = 0; i <= 200; i++) {
+        if (i%2 != 0){
+            console
+            numbers.push(i)
+        }
+       }
+}
+console.log(numbers)
